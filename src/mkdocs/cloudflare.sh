c97deb59f6e80889bfcd91dc8a31e42f0b0419aa
Daniel Ellis <daniel.ellis@ext.esa.int>
2026-02-18 13:00:24 +0000

    Allowing previews builds of pull request branches (permissions update)

--- src/mkdocs/cloudflare.sh
+++ src/mkdocs/cloudflare.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$CF_PAGES_BRANCH" == "production" ]; then
-	echo "We are on the production, reusing actions build 'docs'
+	echo "We are on the production, reusing actions build 'docs'"
 else
-	pip install src/mkdocs/requirements.txt;
+	pip install -r src/mkdocs/requirements.txt;
 	mkdocs build --clean -f src/mkdocs/mkdocs.yml;
 	rm -rf docs; 
-	cp -r src/mkdocs/site/docs;
+	cp -r src/mkdocs/site docs;
 	rm -rf src/ 
 fi
